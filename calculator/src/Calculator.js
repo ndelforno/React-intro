45d7767574ac11cad32b7e2ca6fc604e98e8641f
Ndelforno <nicolas.delforno@gmail.com>
2018-11-28 17:46:59 -0500

    calculator function done

--- calculator/src/Calculator.js
+++ calculator/src/Calculator.js
@@ -5,9 +5,35 @@ class Calculator extends Component{
   constructor (props){
     super()
     this.state = {
-      num1:"" ,
-      num2: ""
+      num1: "" ,
+      num2: "",
+      result:""
     }
+  }
+
+  saveNum1(e) {
+    console.log('this is:', this);
+    console.log(e.target.value)
+    this.setState({
+      num1: e.target.value
+    })
+  }
+
+  saveNum2(e) {
+    console.log('this is:', this);
+    console.log(e.target.value)
+    this.setState({
+      num2: e.target.value
+    })
+  }
+
+  addNumbers(e){
+    console.log(this.state.num1)
+    console.log(this.state.num2)
+    this.setState({
+      result: Number(this.state.num1) + Number(this.state.num2)
+    })
+    console.log(this.state.result)
   }
 
   render(){
@@ -16,15 +42,17 @@ class Calculator extends Component{
         <h1>Add with React!</h1>
 
         <div className="add">
-          <input type="text"
+          <input type="number"
           placeholder="enter a number"
+          onChange={(e) => this.saveNum1(e)}
           value={this.state.num1} />
           <span>+</span>
-          <input type="text"
+          <input type="number"
           placeholder="enter a number"
+          onChange={(e) => this.saveNum2(e)}
           value={this.state.num2} />
-          <span>=</span>
-          <h3>Addition results go here!</h3>
+          <button onClick={(e) => this.addNumbers(e)}>Result </button>
+          <h3>{this.state.result}</h3>
         </div>
       </div>
     )
