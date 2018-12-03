5aeab92c79a87c7c1c1b1081877373d935d00f12
Ndelforno <nicolas.delforno@gmail.com>
2018-12-03 13:53:57 -0500

    Home component created

--- dentist-website/src/App.js
+++ dentist-website/src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Dentist Website</h1>
-        <p>
-          Welcome to my dentist website.
-        </p>
+        <Home></Home>
       </div>
     )
   }
