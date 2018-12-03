90330683f4d375325ba582e22e9dec0ae726d779
Ndelforno <nicolas.delforno@gmail.com>
2018-12-03 17:12:39 -0500

    fetch request working, but bug for city and weather

--- blog_post/src/About.js
+++ blog_post/src/About.js
@@ -2,61 +2,60 @@ import React, { Component } from 'react';
 
 class About extends Component {
 
-    constructor(props){
-      super(props)
-      this.state = {
-        currentTemp: "",
-        highTemp: "",
-        lowTemp: "",
-        currentWeather: "",
-        city: ""
-      }
+  constructor(props){
+    super(props)
+    this.state = {
+      value:"",
+      currentTemp: "",
+      highTemp: "",
+      lowTemp: "",
+      currentWeather: "",
+      city: ""
     }
+  }
 
-    componentDidMount(){
-      var zip = event.target.value
-      var base = this
-      let weatherApi = 'http://api.openweathermap.org/data/2.5/weather?zip={zip},us&appid=052f26926ae9784c2d677ca7bc5dec98';
-      fetch(weatherApi)
-        .then((response) => {
-          return response.json()
-        }).then((json) => {
-          base.setState({
-            currentTemp: json.main.temp,
-            highTemp: json.main.temp_max,
-            lowTemp: json.main.temp_min,
-            currentWeather: json.weather[0].description,
-            city: json.name});
-        }).catch((ex) => {
-          console.log('an error occured while parsing!', ex)
-        })
-    }
+  componentDidMount(){
+    console.log(this.state.value)
+    var base = this
+    let weatherApi = `http://api.openweathermap.org/data/2.5/weather?zip=${base.state.value},us&appid=052f26926ae9784c2d677ca7bc5dec98`;
+    fetch(weatherApi)
+      .then((response) => {
+        return response.json()
+      }).then((json) => {
+        base.setState({
+          currentTemp: json.main.temp,
+          highTemp: json.main.temp_max,
+          lowTemp: json.main.temp_min,
+          currentWeather: json.weather[0].description,
+          city: json.main.name});
+      }).catch((ex) => {
+        console.log('an error occured while parsing!', ex)
+      })
+    console.log(this.city)
+  }
 
-render() {
-    let temperature = this.state.currentTemp
-    let maxtemp = this.state.highTemp
-    let mintemp = this.state.lowTemp
-    let description = this.currentWeather
-    let city = this.city
+  handleChange(e) {
+    this.setState({value: e.target.value});
+  }
+
+
+
+  render(){
     return (
       <div>
         <h1>About</h1>
-        <p>
-        I you want I can tell you what is the weather right now in your area
+        <p>I you want I can tell you what is the weather right now in your area</p>
         <p>Please enter your zip code for the weather:</p>
-        <form>
-          <label>
-            Postal code:
-            <input type="text" name="Postal Code" />
-          </label>
-          <input type="submit" value="Get my weather" />
-        </form>
-        </p>
-          <p> Current temperature: {temperature}</p>
-          <p> Max temperature: {maxtemp}</p>
-          <p> Min temperature: {mintemp}</p>
-          <p> Weather: {description}</p>
-          <p> City: {city}</p>
+        <input type="text"
+          placeholder="enter your zip Code"
+          onChange={(e) => this.handleChange(e)}
+          value={this.state.value} />
+          <button onClick={(e) => this.componentDidMount(e)}>Get my weather ! </button>
+          <p> Current temperature: {this.state.currentTemp}</p>
+          <p> Max temperature: {this.state.highTemp}</p>
+          <p> Min temperature: {this.state.lowTemp}</p>
+          <p> Weather: {this.currentWeather}</p>
+          <p> City: {this.city}</p>
       </div>
     )
   }
