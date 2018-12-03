61642de046545fedd56d7c5b076361787a899f7e
Ndelforno <nicolas.delforno@gmail.com>
2018-12-03 13:46:06 -0500

    welcome page setup

--- dentist-website/src/App.js
+++ dentist-website/src/App.js
@@ -5,23 +5,13 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Dentist Website</h1>
+        <p>
+          Welcome to my dentist website.
+        </p>
       </div>
-    );
+    )
   }
 }
 
